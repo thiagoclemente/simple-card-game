df2b183f17a13cc207f35a90d0a759fefc5279fd
Thiago Clemente <thiago.clemente@gmail.com>
2023-04-08 10:47:55 -0300

    first commit

--- Simple Card Game/ContentView.swift
+++ Simple Card Game/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var player1Card = "back"
+    @State var player2Card = "back"
+    
+    @State var player1Score = 0
+    @State var player2Score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(player1Card)
+                    Spacer()
+                    Image(player2Card)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player 1")
+                        Text(String(player1Score))
+                    }
+                    Spacer()
+                    VStack {
+                        Text("Player 2")
+                        Text(String(player2Score))
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func deal() {
+        let player1CardValue = Int.random(in: 2...14);
+        player1Card = "card" + String(player1CardValue)
+        
+        let player2CardValue = Int.random(in: 2...14);
+        player2Card = "card" + String(player2CardValue)
+        
+        if player1CardValue > player2CardValue {
+            player1Score += 1
+        }else if player2CardValue > player1CardValue {
+            player2Score += 1
+        }
     }
 }
 
