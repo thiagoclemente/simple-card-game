1d1c33333cb9921f817befefb6c8a2c55388515f
Thiago Clemente <thiago.clemente@gmail.com>
2023-04-08 10:43:14 -0300

    Initial Commit

---
+++ Simple Card Game/Simple_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Card_GameApp.swift
+//  Simple Card Game
+//
+//  Created by Thiago on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
